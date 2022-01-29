833a7ab124d3893720151c5592d552042c13427e
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2022-01-29 13:08:56 +0530

    Todo basic structure developed Added Header and Form into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Form from "./components/Form";
+import Header from "./components/Header";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Enter some task...." />
+    </div>
+  );
+};
+
+export default Form;
